023caaf40a86ed256a7bda68267bfefe6cde92df
VictorSamp <victor.msampaio@outlook.com.br>
2021-02-11 09:09:16 -0300

    Criando repositorio e adicionando exercicio 'dividindo x por y'

---
+++ README.md
@@ -0,0 +1,16 @@
+<center><h1>Desafios bootcamp "Avanade Fullstack Developer"</h1></center>
+
+<p align="center">
+<img src="./assets/logo-avanade.png" alt="" height="120" /><img src="./assets/logo-DIO.png" alt="" height="120" />
+</p>
+
+
+
+<p>Nesse repositório estão presentes todos os desafios do bootcamp "Avanade Fullstack Developer" fornecido pela Avanade em parceria com a Digital Innovation One.</p>
+
+O bootcamp pode ser encontrado nesse link: [Avanade Fullstack Developer](https://web.digitalinnovation.one/track/avanade-fullstack-developer?tab=path)
+
+### Tecnologias Implementadas:
+
+<left><img src="./assets/icone_csharp.png" alt="csharp" height="50"/><img src="./assets/icone-javascript.png" alt="javascript" height="50"/></left>
+
---
+++ dividindo-x-por-y/dividindo-x-por-y/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace dividindo_x_por_y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadePares, valorX, valorY;
+            string[] valores;
+            double divisao;
+
+            quantidadePares = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidadePares; i++)
+            {
+                valores = Console.ReadLine().Split(' ');
+                valorX = int.Parse(valores[0]);
+                valorY = int.Parse(valores[1]);
+
+                if (valorY == 0.0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    divisao = (double)valorX / valorY;
+                    Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
